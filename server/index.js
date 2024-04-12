af112e4313ea7910437adc982f918c9d8e5dd9ea
Amit Singh <iamitsinghthakur123@gmail.com>
2024-04-12 16:59:01 +0530

    changed model name to models

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const authRoutes = require("./routes/auth");
-const messageRoutes = require("./routes/messages");
+const authRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messagesRoutes");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();

de384d9f7c8055e62fbc4cadd5ffc8d2eea9be07
Amit Singh <iamitsinghthakur123@gmail.com>
2024-04-12 22:26:34 +0530

    Changed  vercel.json added headers key from official website of vercel

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
 export const host =
-  "https://real-time-chat-app-using-socket-io-blond.vercel.app/";
+  "https://real-time-chat-app-using-socket-io-blond.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
